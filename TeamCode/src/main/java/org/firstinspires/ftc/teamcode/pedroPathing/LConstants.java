55678c379251a578f86ba92449e4cca2d9c52664
10650biohazards <hazmat10650@gmail.com>
2025-07-14 14:39:54 -0400

    IT WORKS
    PedroPathing now uses the three odometry wheel + IMU setup
    LocalizationTest proves it works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/LConstants.java
@@ -6,31 +6,48 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = 0.006431607007;
-        ThreeWheelConstants.strafeTicksToInches = 0.006431607007;
-        ThreeWheelConstants.turnTicksToInches = .001989436789;
-        ThreeWheelConstants.leftY = 1;
-        ThreeWheelConstants.rightY = -1;
-        ThreeWheelConstants.strafeX = -2.5;
-        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
-        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
-        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
 
-        TwoWheelConstants.forwardTicksToInches = 0.006431607007;
-        TwoWheelConstants.strafeTicksToInches = 0.006431607007;
-        TwoWheelConstants.forwardEncoder_HardwareMapName = "leftFront";
-        TwoWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
-        TwoWheelConstants.forwardEncoderDirection = Encoder.REVERSE;
-        TwoWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+        ThreeWheelIMUConstants.forwardTicksToInches = .001989436789;
+        ThreeWheelIMUConstants.strafeTicksToInches = .001989436789;
+        ThreeWheelIMUConstants.turnTicksToInches = .001989436789;
+        ThreeWheelIMUConstants.leftY = 3.5;
+        ThreeWheelIMUConstants.rightY = -3.5;
+        ThreeWheelIMUConstants.strafeX = -4.5;
+        ThreeWheelIMUConstants.leftEncoder_HardwareMapName = "leftEncoder";
+        ThreeWheelIMUConstants.rightEncoder_HardwareMapName = "rightEncoder";
+        ThreeWheelIMUConstants.strafeEncoder_HardwareMapName = "strafeEncoder";
+        ThreeWheelIMUConstants.leftEncoderDirection = Encoder.REVERSE;
+        ThreeWheelIMUConstants.rightEncoderDirection = Encoder.REVERSE;
+        ThreeWheelIMUConstants.strafeEncoderDirection = Encoder.REVERSE;
+        ThreeWheelIMUConstants.IMU_HardwareMapName = "imu";
+        ThreeWheelIMUConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.RIGHT, RevHubOrientationOnRobot.UsbFacingDirection.UP);
 
-        TwoWheelConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.RIGHT, RevHubOrientationOnRobot.UsbFacingDirection.UP);
-       // TwoWheelConstants.
-
-        TwoWheelConstants.forwardY = 1;
-        TwoWheelConstants.strafeX = -2.5;
+//
+//        ThreeWheelConstants.forwardTicksToInches = 0.006431607007;
+//        ThreeWheelConstants.strafeTicksToInches = 0.006431607007;
+//        ThreeWheelConstants.turnTicksToInches = .001989436789;
+//        ThreeWheelConstants.leftY = 1;
+//        ThreeWheelConstants.rightY = -1;
+//        ThreeWheelConstants.strafeX = -2.5;
+//        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
+//        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
+//        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
+//        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
+//        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
+//        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+//
+//        TwoWheelConstants.forwardTicksToInches = 0.006431607007;
+//        TwoWheelConstants.strafeTicksToInches = 0.006431607007;
+//        TwoWheelConstants.forwardEncoder_HardwareMapName = "leftFront";
+//        TwoWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
+//        TwoWheelConstants.forwardEncoderDirection = Encoder.REVERSE;
+//        TwoWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+//
+//        TwoWheelConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.RIGHT, RevHubOrientationOnRobot.UsbFacingDirection.UP);
+//       // TwoWheelConstants.
+//
+//        TwoWheelConstants.forwardY = 1;
+//        TwoWheelConstants.strafeX = -2.5;
     }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/ForwardTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/ForwardTuner.java
@@ -37,7 +37,7 @@ public class ForwardTuner extends OpMode {
 
     private Telemetry telemetryA;
 
-    public static double DISTANCE = 5;
+    public static double DISTANCE = 48;
 
     /**
      * This initializes the PoseUpdater as well as the FTC Dashboard telemetry.
